197abaedbd5efb4d3f9ba82e3b24c2fcc009e78f
Ankit Kumar <kumarankit0520@gmail.com>
2025-02-01 13:57:58 +0530

    add the change 1

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import test from 'node:test';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    // { path: '', component: TestComponent }
+];
