ec3531701c0e9350cad53f804a0722cbd776a793
Ankit Kumar <kumarankit0520@gmail.com>
2025-02-07 11:39:43 +0530

    Add learning Lessons

--- src/app/new-component/new-component.component.ts
+++ src/app/new-component/new-component.component.ts
@@ -1,11 +1,29 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms'; 
 
 @Component({
   selector: 'app-new-component',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './new-component.component.html',
   styleUrl: './new-component.component.css'
 })
 export class NewComponentComponent {
+  title:string=""
+  age:number=23
+  sex:string="M"
 
+  gender:string="M";
+
+  constructor(){
+
+  }
+  showmsg(message:string){
+    if(message!=null && message=='')
+      {
+        alert(message)
+      }
+      else{
+        alert('blank')
+      }
+  }
 }
